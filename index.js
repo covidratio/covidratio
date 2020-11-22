2d1b22e7c060fa0165f333666e802ec6026c150b
David Barratt <david@davidwbarratt.com>
2020-11-22 08:33:11 -0500

    Use next handler

--- index.js
+++ index.js
@@ -1,55 +1,7 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+import { getEventHandler } from '@cinematix/next-cloudflare';
+import routesManifest from './.next/routes-manifest.json';
 
-/**
- * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
- * 2. we will return an error message on exception in your Response rather
- *    than the default 404.html page.
- */
-const DEBUG = false;
-
-async function handleEvent(event) {
-  const options = {};
-
-  try {
-    if (DEBUG) {
-      // customize caching
-      options.cacheControl = {
-        bypassCache: true,
-      };
-    }
-    return await getAssetFromKV(event, options);
-  } catch (error) {
-    // if an error is thrown try to serve the asset at 404.html
-    if (!DEBUG) {
-      try {
-        const notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: (req) => new Request(`${new URL(req.url).origin}/404.html`, req),
-        });
-
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 });
-      } catch (e) {
-        // Silence is golden.
-      }
-    }
-
-    return new Response(error.message || error.toString(), { status: 500 });
-  }
-}
+const handleEvent = getEventHandler(routesManifest);
 
 // eslint-disable-next-line no-restricted-globals
-addEventListener('fetch', (event) => {
-  try {
-    event.respondWith(handleEvent(event));
-  } catch (e) {
-    if (DEBUG) {
-      event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      );
-    }
-    event.respondWith(new Response('Internal Error', { status: 500 }));
-  }
-});
+addEventListener('fetch', handleEvent);
